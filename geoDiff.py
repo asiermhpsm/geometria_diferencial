a62ec5cdc6b29ad107b7c582a865d18bacb618b0
asiermhpsm <118305795+asiermhpsm@users.noreply.github.com>
2023-10-20 12:33:08 +0200

    Añadidos ejemplos y parametros opcionales a geoDiff

--- geoDiff.py
+++ geoDiff.py
@@ -220,7 +220,7 @@ def segundaFormaFundamental_pt_xyz(parametrizacion, u, v, x0, y0, z0, efg=None):
 CURVATURA DE GAUSS
 -------------------------------------------------------------------------------
 """
-def curvaturaGauss(parametrizacion, u, v):
+def curvaturaGauss(parametrizacion, u, v, EFG=None, efg=None):
     """
     Retorna la curvatura de Gauss
     No se hacen comprobaciones de tipo
@@ -229,12 +229,14 @@ def curvaturaGauss(parametrizacion, u, v):
     parametrizacion     parametrizacion de superficie (lista de longitud 3 con funciones)
     u                   primera variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
     v                   segunda variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g
     """
-    E, F, G = primeraFormaFundamental(parametrizacion, u, v)
-    e, f, g = segundaFormaFundamental(parametrizacion, u, v)
+    E, F, G = EFG if EFG else primeraFormaFundamental(parametrizacion, u, v)
+    e, f, g = efg if efg else segundaFormaFundamental(parametrizacion, u, v)
     return sp.simplify((e*g - f**2) / (E*G - F**2))
 
-def curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
+def curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv=None, EFG=None, efg=None):
     """
     Retorna la curvatura de Gauss en un punto descrito por u, v
     No se hacen comprobaciones de tipo
@@ -246,15 +248,17 @@ def curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
     u0                  valor u del punto
     v0                  valor v del punto
     curv                parametro opcional por si ya se tiene calculada la curvatura de Gauss
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G en el punto deseado
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g en el punto deseado
     """
     if curv:
         return sp.N(curv.subs([[u, u0],[v,v0]]))
     
-    E_pt, F_pt, G_pt = primeraFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
-    e_pt, f_pt, g_pt = segundaFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    E_pt, F_pt, G_pt = EFG if EFG else primeraFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    e_pt, f_pt, g_pt = efg if efg else segundaFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
     return (e_pt*g_pt - f_pt**2) / (E_pt*G_pt - F_pt**2)
 
-def curvaturaGauss_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
+def curvaturaGauss_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None, EFG=None, efg=None):
     """
     Retorna la curvatura de Gauss en un punto descrito por x, y, z
     No se hacen comprobaciones de tipo
@@ -267,9 +271,11 @@ def curvaturaGauss_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
     y0                  valor y del punto
     z0                  valor z del punto
     curv                parametro opcional por si ya se tiene calculada la curvatura de Gauss
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G en el punto deseado
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g en el punto deseado
     """
     u0, v0 = xyz_to_uv(parametrizacion, u, v, x0, y0, z0)
-    return curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv)
+    return curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv, EFG, efg)
 
 
 """
@@ -277,7 +283,7 @@ def curvaturaGauss_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
 CURVATURA MEDIA
 -------------------------------------------------------------------------------
 """
-def curvaturaMedia(parametrizacion, u, v):
+def curvaturaMedia(parametrizacion, u, v, EFG=None, efg=None):
     """
     Retorna la curvatura media
     No se hacen comprobaciones de tipo
@@ -286,12 +292,14 @@ def curvaturaMedia(parametrizacion, u, v):
     parametrizacion     parametrizacion de superficie (lista de longitud 3 con funciones)
     u                   primera variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
     v                   segunda variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g
     """
-    E, F, G = primeraFormaFundamental(parametrizacion, u, v)
-    e, f, g = segundaFormaFundamental(parametrizacion, u, v)
+    E, F, G = EFG if EFG else primeraFormaFundamental(parametrizacion, u, v)
+    e, f, g = efg if efg else segundaFormaFundamental(parametrizacion, u, v)
     return sp.simplify((e*G + g*E - 2*f*F) / (2*(E*G - F**2)))
 
-def curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
+def curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, curv=None, EFG=None, efg=None):
     """
     Retorna la curvatura media en un punto descrito por u, v
     No se hacen comprobaciones de tipo
@@ -303,15 +311,17 @@ def curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
     u0                  valor u del punto
     v0                  valor v del punto
     curv                parametro opcional por si ya se tiene calculada la curvatura de media
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G en el punto deseado
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g en el punto deseado
     """
     if curv:
         return sp.N(curv.subs([[u, u0],[v,v0]]))
     
-    E_pt, F_pt, G_pt = primeraFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
-    e_pt, f_pt, g_pt = segundaFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    E_pt, F_pt, G_pt = EFG if EFG else primeraFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    e_pt, f_pt, g_pt = efg if efg else segundaFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
     return (e_pt*G_pt + g_pt*E_pt - 2*f_pt*F_pt) / (2*(E_pt*G_pt - F_pt**2))
 
-def curvaturaMedia_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
+def curvaturaMedia_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None, EFG=None, efg=None):
     """
     Retorna la curvatura media en un punto descrito por x, y, z
     No se hacen comprobaciones de tipo
@@ -324,9 +334,11 @@ def curvaturaMedia_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
     y0                  valor y del punto
     z0                  valor z del punto
     curv                parametro opcional por si ya se tiene calculada la curvatura de media
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G en el punto deseado
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g en el punto deseado
     """
     u0, v0 = xyz_to_uv(parametrizacion, u, v, x0, y0, z0)
-    return curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, curv)
+    return curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, curv, EFG, efg)
 
 
 """
@@ -334,7 +346,7 @@ def curvaturaMedia_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
 CURVATURAS PRINCIPALES
 -------------------------------------------------------------------------------
 """
-def curvaturasPrincipales(parametrizacion, u, v):
+def curvaturasPrincipales(parametrizacion, u, v, K=None, H=None):
     """
     Retorna las curvaturas principales como tupla
     No se hacen comprobaciones de tipo
@@ -343,13 +355,17 @@ def curvaturasPrincipales(parametrizacion, u, v):
     parametrizacion     parametrizacion de superficie (lista de longitud 3 con funciones)
     u                   primera variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
     v                   segunda variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
+    K                   parametro opcional por si se tiene ya calculada cruvatura Gauss
+    H                   parametro opcional por si se tiene ya calculada curvatura Media
     """
-    K = curvaturaGauss(parametrizacion, u, v)
-    H = curvaturaMedia(parametrizacion, u, v)
+    E, F, G = primeraFormaFundamental(parametrizacion, u, v)
+    e, f, g = segundaFormaFundamental(parametrizacion, u, v)
+    K = curvaturaGauss(parametrizacion, u, v, EFG=(E, F, G), efg=(e,f,g))
+    H = curvaturaMedia(parametrizacion, u, v, EFG=(E, F, G), efg=(e,f,g))
     raiz = sp.sqrt(H**2 - K)
     return sp.simplify(H + raiz), sp.simplify(H - raiz)
 
-def curvaturasPrincipales_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
+def curvaturasPrincipales_pt_uv(parametrizacion, u, v, u0, v0, curv=None, K=None, H=None):
     """
     Retorna como tupla las curvaturas principales en un punto descrito como u,v
     No se hacen comprobaciones de tipo
@@ -361,16 +377,20 @@ def curvaturasPrincipales_pt_uv(parametrizacion, u, v, u0, v0, curv=None):
     u0                  valor u del punto
     v0                  valor v del punto
     curv                parametro opcional por si ya se tienen calculadas las curvaturas principales
+    K                   parametro opcional por si se tiene ya calculada cruvatura Gauss en el punto deseado
+    H                   parametro opcional por si se tiene ya calculada curvatura Media en el punto deseado
     """
     if curv:
         return ( sp.N(curv[0].subs([[u, u0],[v,v0]])), sp.N(curv[1].subs([[u, u0],[v,v0]])))
     
-    K_pt = curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0)
-    H_pt = curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0)
+    E_pt, F_pt, G_pt = primeraFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    e_pt, f_pt, g_pt = segundaFormaFundamental_pt_uv(parametrizacion, u, v, u0, v0)
+    K_pt = curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, EFG=(E_pt,F_pt, G_pt), efg=(e_pt,f_pt,g_pt))
+    H_pt = curvaturaMedia_pt_uv(parametrizacion, u, v, u0, v0, EFG=(E_pt,F_pt, G_pt), efg=(e_pt,f_pt,g_pt))
     raiz = sp.sqrt(H_pt**2 - K_pt)
     return H_pt + raiz, H_pt - raiz
 
-def curvaturasPrincipales_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
+def curvaturasPrincipales_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None, K=None, H=None):
     """
     Retorna como tupla las curvaturas principales en un punto descrito como x, y, z
     No se hacen comprobaciones de tipo
@@ -382,9 +402,11 @@ def curvaturasPrincipales_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
     u0                  valor u del punto
     v0                  valor v del punto
     curv                parametro opcional por si ya se tienen calculadas las curvaturas principales
+    K                   parametro opcional por si se tiene ya calculada cruvatura Gauss en el punto deseado
+    H                   parametro opcional por si se tiene ya calculada curvatura Media en el punto deseado
     """
     u0, v0 = xyz_to_uv(parametrizacion, u, v, x0, y0, z0)
-    return curvaturasPrincipales_pt_uv(parametrizacion, u, v, u0, v0, curv)
+    return curvaturasPrincipales_pt_uv(parametrizacion, u, v, u0, v0, curv, K, H)
 
 
 """
@@ -392,7 +414,7 @@ def curvaturasPrincipales_pt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None):
 CLASIFICACION DE UN PUNTO
 -------------------------------------------------------------------------------
 """
-def clasicPt_uv(parametrizacion, u, v, u0, v0):
+def clasicPt_uv(parametrizacion, u, v, u0, v0, curv=None, EFG=None, efg=None):
     """
     Imprime la clasificacion de una superficie en un punto descrito con x, y, z
     No se hacen comprobaciones de tipo
@@ -403,8 +425,11 @@ def clasicPt_uv(parametrizacion, u, v, u0, v0):
     v                   segunda variable de parametrizacion ( clase sp.Symbol, resultado de sp.symbols() )
     u0                  valor u del punto
     v0                  valor v del punto
+    curv                parametro opcional por si ya se tiene calculada la curvatura de Gauss
+    EFG                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados E, F, G en el punto deseado
+    efg                 parametro opcional (tupla de longitud 3) por si se tienen ya calculados e, f, g en el punto deseado
     """
-    K_pt = curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0)
+    K_pt = curvaturaGauss_pt_uv(parametrizacion, u, v, u0, v0, curv, EFG, efg)
     if K_pt > 0:
         print('Eliptico')
     elif K_pt < 0:
@@ -412,7 +437,7 @@ def clasicPt_uv(parametrizacion, u, v, u0, v0):
     elif K_pt == 0:
         print('Parabólico o planar')
 
-def clasicPt_xyz(parametrizacion, u, v, x0, y0, z0):
+def clasicPt_xyz(parametrizacion, u, v, x0, y0, z0, curv=None, EFG=None, efg=None):
     """
     Imprime la clasificacion de una superficie en un punto descrito con x, y, z
     No se hacen comprobaciones de tipo
@@ -426,7 +451,7 @@ def clasicPt_xyz(parametrizacion, u, v, x0, y0, z0):
     z0                  valor z del punto
     """
     u0, v0 = xyz_to_uv(parametrizacion, u, v, x0, y0, z0)
-    return clasicPt_uv(parametrizacion, u, v, u0, v0)
+    return clasicPt_uv(parametrizacion, u, v, u0, v0, curv, EFG, efg)
 
 
 """
