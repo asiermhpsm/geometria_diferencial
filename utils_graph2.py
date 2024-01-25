96273c3f81fabc2a1f043c01b6863126e50a083b
asiermhpsm <118305795+asiermhpsm@users.noreply.github.com>
2024-01-25 20:59:41 +0100

    Update utils_graph2.py

--- utils_graph2.py
+++ utils_graph2.py
@@ -9,7 +9,7 @@ def grafica_sup_param(sup, u, v, limite_inf_u, limite_sup_u, limite_inf_v, limit
     u_values, v_values = np.meshgrid(u_values, v_values)
     x, y, z = parametric_surface(u_values, v_values)
     #mlab.mesh(x, y, z, colormap='viridis')
-    mlab.mesh(x, y, z, color=(0, 0, 1))
+    mlab.mesh(x, y, z, color=(0.2980392156862745, 0.4470588235294118, 0.6901960784313725))
 
 def grafica_sup_ec(sup, x, y, z, limite_inf_x, limite_sup_x, limite_inf_y, limite_sup_y):
     parametric_surface = sp.lambdify((x, y, z), sup, 'numpy')
@@ -23,9 +23,9 @@ def grafica_punto(punto):
 def grafica_vector(inicio, vector):
     mlab.quiver3d(*inicio, *vector, color=(0, 0, 1), scale_factor=1)
 
-mlab.plot3d([0, 2], [0, 0], [0, 0], color=(1, 0, 0), tube_radius=None)
+"""mlab.plot3d([0, 2], [0, 0], [0, 0], color=(1, 0, 0), tube_radius=None)
 mlab.plot3d([0, 0], [0, 2], [0, 0], color=(0, 1, 0), tube_radius=None)
-mlab.plot3d([0, 0], [0, 0], [0, 2], color=(0, 0, 1), tube_radius=None)
+mlab.plot3d([0, 0], [0, 0], [0, 2], color=(0, 0, 1), tube_radius=None)"""
 
 u, v = sp.symbols('u v')
 parametrizacion = (sp.cos(u)*sp.cos(v), sp.cos(u)*sp.sin(v) , sp.sin(u))
@@ -42,5 +42,5 @@ vector = np.array([0, 0, 1])
 #grafica_vector(punto, vector)
 
 mlab.axes()
-mlab.orientation_axes()
+#mlab.orientation_axes()
 mlab.show()
